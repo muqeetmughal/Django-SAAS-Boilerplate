d7b1617b66c6f16227096deb8a9e66d39cbb9c03
muqeetmughal <muqeetmughal786@gmail.com>
2023-06-28 00:30:34 +0500

    :construction_worker: admin for tenants app added

--- tenants/admin.py
+++ tenants/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django_tenants.admin import TenantAdminMixin
+from tenants.models import Tenant, Domain
 
-# Register your models here.
+
+class DomainInline(admin.TabularInline):
+    model = Domain
+    max_num = 1
+
+
+@admin.register(Tenant)
+class TenantAdmin(TenantAdminMixin, admin.ModelAdmin):
+    list_display = ("email", "paid_until", "on_trial", "is_active", "is_verified")
+    inlines = [DomainInline]
+
+
+admin.site.register(Domain)
